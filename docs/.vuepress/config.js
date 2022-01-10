5f7db58ac68048f56755b00629526a49f95f6b5e
kakarote <337863414@qq.com>
2022-01-10 15:38:17 +0800

    修改git地址

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -105,7 +105,7 @@ module.exports = {
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
-    repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+    repo: 'kakarote/zwDoc', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
@@ -146,8 +146,8 @@ module.exports = {
 
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
     author: {
-      name: 'xugaoyi', // 必需
-      link: 'https://github.com/xugaoyi', // 可选的
+      name: 'kakarote', // 必需
+      link: 'https://github.com/kakarote/zwDoc.git', // 可选的
     },
 
     // 博主信息，显示在首页侧边栏
@@ -169,7 +169,7 @@ module.exports = {
         {
           iconClass: 'icon-github',
           title: 'GitHub',
-          link: 'https://github.com/xugaoyi',
+          link: 'https://github.com/kakarote/zwDoc.git',
         },
         {
           iconClass: 'icon-erji',
@@ -183,7 +183,7 @@ module.exports = {
     footer: {
       createYear: 2019, // 博客创建年份
       copyrightInfo:
-        'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+        'Evan Xu | <a href="https://github.com/kakarote/zwDoc.git/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
     },
 
     // 插入hmtl(广告)模块
